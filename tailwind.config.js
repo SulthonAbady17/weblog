c5525090627c1ed1037c5f131177942504944316
Adam Davala Lazuardi <adamdavala2@gmail.com>
2022-04-11 16:50:58 +0700

    Adding custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {},
+    colors: {
+      one: '#012467',
+      two: '#1053BA',
+      three: '#FA8072',
+      four: '#FFDAB9'
+    }
   },
   plugins: [],
 }
